6d721f13288fc5be1ba63599592bb2a35413e4d5
Phoenix Nicholson <phoenix.nicholson2008@gmail.com>
2022-02-16 12:00:10 -0800

    wrapped project in provider, mapped through userEntries

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,11 @@ import Header from './components/Header/Header';
 import Entry from './views/Entry';
 
 import './App.css';
+import { PlannerProvider } from './context/PlannerContext';
 
 export default function App() {
   return (
-    <> {/* TODO: Add PlannerProvider */}
+    <PlannerProvider>
       <Header />
       <BrowserRouter>
         <Switch>
@@ -22,6 +23,6 @@ export default function App() {
           </Route>
         </Switch>
       </BrowserRouter>
-    </>
+    </PlannerProvider>
   );
 }
